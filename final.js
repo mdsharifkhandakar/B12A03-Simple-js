8ee893ac3bd1f6604265640395f9b4a92c5acbde
mdsharifkhandakar <mdsharifkhandakar009@gmail.com>
2025-08-29 19:21:34 +0600

    added the problem 6

--- final.js
+++ final.js
@@ -50,3 +50,14 @@ var student = { name: "jhanku", roll: 1014, department: "cse" };
 var email =
   student.name + student.roll + "." + student.department + "@ph.ac.bd";
 console.log(email);
+
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+//write your code here
+var currentSalary = startingSalary;
+for (var i = 1; i <= experience; i++) {
+  currentSalary = currentSalary + currentSalary * 0.05;
+}
+
+console.log(currentSalary.toFixed(2));
