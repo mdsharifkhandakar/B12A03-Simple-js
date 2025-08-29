8f19d3453610dc22194480f222cf28e86852b8aa
mdsharifkhandakar <mdsharifkhandakar009@gmail.com>
2025-08-29 19:15:27 +0600

    added the problem 2

--- problem2.js
+++ problem2.js
@@ -1,11 +1,3 @@
-/** Problem -01 ( Divide the Asset ) */
-var area = 2060;
-//write your code here
-var rahim = area / 2;
-var karim = area / 2;
-console.log(rahim);
-console.log(karim);
-
 /** Problem -02 ( Cycle or Laptop ) */
 var money = 10000;
 //write your code here
