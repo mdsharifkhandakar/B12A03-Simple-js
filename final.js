235b35c7423c4074e048f77a77f262e60778eabb
mdsharifkhandakar <mdsharifkhandakar009@gmail.com>
2025-08-29 19:20:04 +0600

    added the problem 5

--- final.js
+++ final.js
@@ -43,3 +43,10 @@ if (fileName[0] === "#") {
 } else {
   console.log("Delete");
 }
+
+/** Problem 05 - ( PH Email Generator )  */
+var student = { name: "jhanku", roll: 1014, department: "cse" };
+//write your code here
+var email =
+  student.name + student.roll + "." + student.department + "@ph.ac.bd";
+console.log(email);
