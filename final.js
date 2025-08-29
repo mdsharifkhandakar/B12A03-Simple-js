b37969af69705f60e4c6d519045f62da9eb4f555
mdsharifkhandakar <mdsharifkhandakar009@gmail.com>
2025-08-29 19:18:41 +0600

    added the problem 4

--- final.js
+++ final.js
@@ -30,3 +30,16 @@ while (day <= lastDay) {
   }
   day++;
 }
+
+/** Problem 04 - (Delete / Store) */
+var fileName = "pdfData.jpg";
+//write your code here
+if (fileName[0] === "#") {
+  console.log("Store");
+} else if (fileName.endsWith(".pdf")) {
+  console.log("Store");
+} else if (fileName.endsWith(".docx")) {
+  console.log("Store");
+} else {
+  console.log("Delete");
+}
